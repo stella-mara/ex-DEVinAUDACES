e3d345398b230040f41a9091e0d987bab1f7ccc6
Stella <stellamarawendt@gmail.com>
2022-12-09 22:01:01 -0300

    ex 08

---
+++ semana-02/ex08/ex08.js
@@ -0,0 +1,12 @@
+var numero = parseInt(prompt('Informe o número'));
+var raiz = parseInt(prompt('Informe a raiz'));
+
+var soma = numero;
+var resultado = numero.toString();
+
+for(var i = 0; i <9; i++) {
+  soma += raiz;
+  resultado += ',' + soma;
+}
+
+alert('A PA é: ' + resultado);
