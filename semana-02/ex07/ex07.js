c7f14bad6efa7f9cc2ae57805f68c576076bd0b8
Stella <stellamarawendt@gmail.com>
2022-12-09 21:46:08 -0300

    ex 07

---
+++ semana-02/ex07/ex07.js
@@ -0,0 +1,17 @@
+var quantidade = 0
+
+for(var i = 2; i <= 1000; i++){
+    var primo = true
+
+    for(var j = 2; j < i; j++){
+      if(i % j === 0){
+        primo = false;
+      }
+    }
+      if(primo){
+        quantidade +=1;
+        console.log(i);
+      }
+}
+
+console.log('Quantidade: ', quantidade);
