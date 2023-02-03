94cf3d8407d5f69ff5df4f998c7c8893e027bb0e
Stella <stellamarawendt@gmail.com>
2023-02-02 21:48:36 -0300

    Atualizações projeto PIGGY BANK

--- semana-04/conta bancaria/banco.js
+++ semana-04/conta bancaria/banco.js
@@ -1,12 +1,12 @@
 //let = contas = [];
 let contas = [
   {
-    nome: 'Thais Bertoldo',
-    cpf: '999.999.999.99',
-    celular: '(99) 99999-9999',
-    senha: '1',
-    conta: 1,
-    saldo: 100,
+    nome: '',
+    cpf: '',
+    celular: '',
+    senha: '',
+    conta: 0,
+    saldo: 0,
   },
 ];
 
@@ -15,40 +15,40 @@ const formulario = document.getElementById('form-cadastro');
 const enviarFormulario = (event) => {
   event.preventDefault();
 
-// OBTER OS CAMPOS DE SENHA
-const senha = event.target.senha.value
-const confirmacaoSenha = event.target.confirmacaoSenha.value
+  // OBTER OS CAMPOS DE SENHA
+  const senha = event.target.senha.value;
+  const confirmacaoSenha = event.target.confirmacaoSenha.value;
 
-if(senha !== confirmacaoSenha) {
-  alert('As senhas são divergentes. Tente novamente.');
-  return;
-}
+  if (senha !== confirmacaoSenha) {
+    alert('As senhas são divergentes. Tente novamente.');
+    return;
+  }
 
-//ADICIONAR A CONTA NO ARRAY
-const nome = event.target.nome.value;
-const cpf = event.target.cpf.value;
-const celular = event.target.celular.value;
-const conta = new Date().getTime();
-const saldo = 0;
+  //ADICIONAR A CONTA NO ARRAY
+  const nome = event.target.nome.value;
+  const cpf = event.target.cpf.value;
+  const celular = event.target.celular.value;
+  const conta = new Date().getTime();
+  const saldo = 0;
 
-const contaCriada = {
-  conta,
-  nome,
-  cpf,
-  celular,
-  senha,
-  saldo,
+  const contaCriada = {
+    conta,
+    nome,
+    cpf,
+    celular,
+    senha,
+    saldo,
+  };
+  contas.push(contaCriada);
+  alert(`Conta criada com sucesso. Número: ${conta}`);
 };
-contas.push(contaCriada);
-alert(`Conta criada com sucesso. Número: ${conta}`)
-};
 
-formulario.addEventListener('submit', enviarFormulario)
+formulario.addEventListener('submit', enviarFormulario);
 
 //OPERAÇÕES
 //OBTER FORMULÁRIO DE OPERAÇÕES
 
-const formOperacao = document.getElementById('form-operacao')
+const formOperacao = document.getElementById('form-operacao');
 
 //FUNÇÃO DE SAQUE
 
@@ -59,39 +59,38 @@ const sacar = (conta, valor) => {
     if (conta.saldo >= valor) {
       const novoSaldo = conta.saldo - valor;
       conta.saldo = novoSaldo;
-  
-      alert(`Saque efetuado com sucesso! Novo saldo: R$ ${novoSaldo}`)
+
+      alert(`Saque efetuado com sucesso! Novo saldo: R$ ${novoSaldo}`);
       return;
     }
 
-    alert('Saldo insuficiente.')
+    alert('Saldo insuficiente.');
     return;
   }
-    alert('Não foi possível efetuar o saque.')
+  alert('Não foi possível efetuar o saque.');
 };
 
 //FUNÇÃO DE DEPÓSITO
 
-const depositar = (conta, valor)=> {
-  if(valor > 0){
-    const novoSaldo = conta.saldo + valor
-    conta.saldo = novoSaldo
+const depositar = (conta, valor) => {
+  if (valor > 0) {
+    const novoSaldo = conta.saldo + valor;
+    conta.saldo = novoSaldo;
 
-    alert(`Depósito efetuado com sucesso! Novo saldo: R$ ${novoSaldo}`)
+    alert(`Depósito efetuado com sucesso! Novo saldo: R$ ${novoSaldo}`);
     return;
-
   }
-  alert('Não foi possível efeutar o depósito.')
+  alert('Não foi possível efeutar o depósito.');
 };
 
 //FUNÇÃO DE CONSULTAR O VALOR
 
-const consultarSaldo = (conta)=> {
+const consultarSaldo = (conta) => {
   alert(`Saldo atual: ${conta.saldo}`);
 };
 
 //ENVIAR FORMULARIO DE OPERAÇÃO
-const enviarFormularioOperacao = (event)=> {
+const enviarFormularioOperacao = (event) => {
   event.preventDefault();
 
   //Obter valores digitados no formulário
@@ -101,14 +100,14 @@ const enviarFormularioOperacao = (event)=> {
   const senha = event.target.senhaOperacao.value;
 
   //Validar conta e senha
-  const contaAtual = contas.find((c)=> c.conta === conta);
-  if(!contaAtual) {
+  const contaAtual = contas.find((c) => c.conta === conta);
+  if (!contaAtual) {
     alert('Conta inválida!');
     return;
   }
 
   //Validar senha
-  if(contaAtual.senha !== senha) {
+  if (contaAtual.senha !== senha) {
     alert('Senha inválida');
     return;
   }
@@ -125,8 +124,9 @@ const enviarFormularioOperacao = (event)=> {
     case 'saldo':
       consultarSaldo(contaAtual);
       break;
-  
-    default: ('Operação Inválida')
+
+    default:
+      'Operação Inválida';
       break;
   }
 };
@@ -138,23 +138,18 @@ formOperacao.addEventListener('submit', enviarFormularioOperacao);
 //Obter select para adicionar evento de onchange
 
 const operacao = document.getElementById('operacao');
-operacao.addEventListener('change', (event)=>{
+operacao.addEventListener('change', (event) => {
   //Obter o campo de valor do HTML
-  const inputValor = document.getElementById('valor')
+  const inputValor = document.getElementById('valor');
   //Veriica se o valor selecionado é saldo
-  if(event.target.value === 'saldo'){
+  if (event.target.value === 'saldo') {
     //Desabilitar o campo
     inputValor.disabled = true;
     //Limpa o valor digitado
     inputValor.value = '';
     return;
   }
-    inputValor.disabled = false;
+  inputValor.disabled = false;
 });
 
 //FINALIZADO
-
-
-
-
-
