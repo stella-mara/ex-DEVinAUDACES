1ad5c8e7060158bbd45ffc57100e2397eda5fc3a
Stella <stellamarawendt@gmail.com>
2023-03-07 14:45:10 -0300

    Criação do components/menu

--- ex-angular-09/src/app/app.module.ts
+++ ex-angular-09/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CardComponent } from './components/card/card.component';
 import { SerieComponent } from './pages/serie/serie.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { SerieComponent } from './pages/serie/serie.component';
     HomeComponent,
     CardComponent,
     SerieComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
